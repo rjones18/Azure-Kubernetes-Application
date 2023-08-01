f6dd9fa621592cb752b49fe27bc3af5fce3bc357
rjones18 <regjones16@outlook.com>
2023-07-31 21:00:59 -0500

    Update node count

--- main.tf
+++ main.tf
@@ -26,7 +26,7 @@ resource "azurerm_kubernetes_cluster" "example" {
 
   default_node_pool {
     name       = "default"
-    node_count = 1
+    node_count = 2
     vm_size    = "Standard_D2_v2"
     vnet_subnet_id = data.azurerm_subnet.existing.id
   }
