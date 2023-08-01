e78dc37698de26526a6416da7c9e26aece97f9d9
rjones18 <regjones16@outlook.com>
2023-08-01 18:36:11 -0500

    Update Node az

--- main.tf
+++ main.tf
@@ -29,6 +29,7 @@ resource "azurerm_kubernetes_cluster" "example" {
     node_count = 2
     vm_size    = "Standard_D2_v2"
     vnet_subnet_id = data.azurerm_subnet.existing.id
+    availability_zones = ["1", "2"]
   }
 
   network_profile {
